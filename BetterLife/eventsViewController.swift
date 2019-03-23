5e0c7b62386abbf9836bda992544e0fd30475980
Regigi <regina.chau3144@gmail.com>
2019-03-23 18:03:01 -0400

    did community page

---
+++ BetterLife/eventsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  eventsViewController.swift
+//  BetterLife
+//
+//  Created by Regina Chau on 3/23/19.
+//  Copyright © 2019 Vicky Zheng. All rights reserved.
+//
+
+import UIKit
+
+class eventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+view.backgroundColor = UIColor.green
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CellCollectionViewCell.swift
+//  BetterLife
+//
+//  Created by Regina Chau on 3/23/19.
+//  Copyright © 2019 Vicky Zheng. All rights reserved.
+//
+
+import UIKit
+
+class CellCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myImage: UIImageView!
+}
