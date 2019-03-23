ea83962c1bafe527b5a4797327a3ea9735d99bb2
Vicky Zheng <vickyzheng205@gmail.com>
2019-03-23 09:15:23 -0400

    Tabbed App

---
+++ BetterLife/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BetterLife
+//
+//  Created by Vicky Zheng on 3/23/19.
+//  Copyright © 2019 Vicky Zheng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BetterLife/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BetterLife
+//
+//  Created by Vicky Zheng on 3/23/19.
+//  Copyright © 2019 Vicky Zheng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
